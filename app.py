872c87aa2f82636f02add6611a9b4afca754ee4b
Breton Tillinghast <btillinghast@draftkings.com>
2023-10-30 21:09:13 -0400

    Added vscode to gitignore

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 current_place = "start"
 experience = 0
 wealth = 0
+
+
 def ask(prompt, options):
     print(prompt)
     for i, option in enumerate(options):
@@ -15,15 +17,17 @@ def ask(prompt, options):
         except ValueError:
             print("Invalid input, try again.")
 
+
 # Massive game loop
 while True:
     if current_place == "start":
         print("You are in a forest located somewhere in New England. What do you do?")
         while True:
-            choice = ask("Choose an action:", ["Explore the forest", "Follow a path", "Climb a tree", "Check wealth"])
+            choice = ask("Choose an action:", [
+                         "Explore the forest", "Follow a path", "Climb a tree", "Check wealth"])
             if choice == 0:
                 print("You travel through the forest and find a river.")
-                current_place = "river":
+                current_place = "river"
                 experience += 1
                 break
             elif choice == 1:
@@ -40,7 +44,8 @@ while True:
     elif current_place == "river":
         print("You come to a river. What do you do?")
         while True:
-            choice = ask("Choose an action:", ["Cross the river", "Follow the river", "Build a raft", "Turn back", "Check wealth"])
+            choice = ask("Choose an action:", [
+                         "Cross the river", "Follow the river", "Build a raft", "Turn back", "Check wealth"])
             if choice == 0:
                 print("You swim across the river safely.")
                 current_place = "bridge"
@@ -52,7 +57,8 @@ while True:
                 experience += 2
                 break
             elif choice == 2:
-                print("You build a raft, which sinks immediately and you are forced to walk back to the riverbank.")
+                print(
+                    "You build a raft, which sinks immediately and you are forced to walk back to the riverbank.")
                 current_place = "river"
                 experience -= 1
                 break
@@ -66,7 +72,8 @@ while True:
     elif current_place == "bridge":
         print("You come to a bridge over a deep ravine. What do you do?")
         while True:
-            choice = ask("Choose an action:", ["Cross the bridge", "Climb down into the ravine", "Turn back", "Check wealth"])
+            choice = ask("Choose an action:", [
+                         "Cross the bridge", "Climb down into the ravine", "Turn back", "Check wealth"])
             if choice == 0:
                 print("You cross the bridge safely.")
                 current_place = "cave"
@@ -82,7 +89,8 @@ while True:
     elif current_place == "cave":
         print("You find yourself in a dark cave. What do you do?")
         while True:
-            choice = ask("Choose an action:", ["Explore the cave", "Leave the cave", "Check wealth"])
+            choice = ask("Choose an action:", [
+                         "Explore the cave", "Leave the cave", "Check wealth"])
             if choice == 0:
                 print("You explore the cave and find a hidden passage.")
                 current_place = "tunnel"
@@ -98,9 +106,11 @@ while True:
     elif current_place == "treasure":
         print("You find a treasure chest. What do you do?")
         while True:
-            choice = ask("Choose an action:", ["Open the chest", "Leave the chest", "Check wealth"])
+            choice = ask("Choose an action:", [
+                         "Open the chest", "Leave the chest", "Check wealth"])
             if choice == 0:
-                print("You open the chest and find a lot of gold. You decide to circle back and explore the cave you saw earlier.")
+                print(
+                    "You open the chest and find a lot of gold. You decide to circle back and explore the cave you saw earlier.")
                 current_place = "cave"
                 experience += 5
                 wealth += 500
@@ -115,9 +125,11 @@ while True:
     elif current_place == "tunnel":
         print("You find a secret tunnel. What do you do?")
         while True:
-            choice = ask("Choose an action:", ["Follow the tunnel", "Leave the tunnel", "Check wealth"])
+            choice = ask("Choose an action:", [
+                         "Follow the tunnel", "Leave the tunnel", "Check wealth"])
             if choice == 0:
-                print("You follow the tunnel and come across what seems to be a small, rural town.")
+                print(
+                    "You follow the tunnel and come across what seems to be a small, rural town.")
                 current_place = "town"
                 experience += 250
                 break
@@ -131,21 +143,26 @@ while True:
     elif current_place == "town":
         print("Once at this town, what do you do?")
         while True:
-            choice = ask("Choose an action:", ["Explore the town", "Buy some supplies", "Rest in a dorm", "Leave the town", "Check wealth"])
+            choice = ask("Choose an action:", [
+                         "Explore the town", "Buy some supplies", "Rest in a dorm", "Leave the town", "Check wealth"])
             if choice == 0:
-                print("You explore the town and find a cafeteria. You think to yourself, is this some sort of school?")
+                print(
+                    "You explore the town and find a cafeteria. You think to yourself, is this some sort of school?")
                 experience += 1
                 break
             elif choice == 1:
-                print("You buy some food to fill your growling stomach, and store the rest in your pocket.")
+                print(
+                    "You buy some food to fill your growling stomach, and store the rest in your pocket.")
                 wealth -= 50
                 experience += 5
-                print("This cost you 50 coins and you currently have", wealth, "coins remaining.")
+                print("This cost you 50 coins and you currently have",
+                      wealth, "coins remaining.")
             elif choice == 2:
                 print("You rest in a dorm and build up your strength.")
                 wealth -= 50
                 experience += 3
-                print("This cost you 50 coins and you currently have", wealth, "coins remaining.")
+                print("This cost you 50 coins and you currently have",
+                      wealth, "coins remaining.")
             elif choice == 3:
                 print("You leave the school and continue your journey.")
                 current_place = "to be continued"
